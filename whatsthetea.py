9fbe626b8f2f7174f46f7eba3de2cbdf1fc83573
Sai Shruthi Balaji <shruthibalaji2307@gmail.com>
2024-04-20 12:45:18 -0700

    Added methods, requirements.txt

--- whatsthetea.py
+++ whatsthetea.py
@@ -6,19 +6,23 @@ import os
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
+# SETUP
+# Set env variable using export OPENAI_API_KEY=<your_key>
 api_key = os.environ['OPENAI_API_KEY']
 llm = OpenAI(temperature=0, openai_api_key=api_key)
 
+def generate_summary(youtube_url_list):
+    texts = []
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
+
+    for url in youtube_url_list:
+        loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
+        result = loader.load()
+
+        texts.extend(text_splitter.split_documents(result))
+    chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=False)
+    return chain.run(texts)
+
 youtube_url_list = ["https://www.youtube.com/watch?v=AXq0QHUwmh8", "https://www.youtube.com/watch?v=EwHrjZxAT7g"]
-
-texts = []
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
-
-for url in youtube_url_list:
-    loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
-    result = loader.load()
-
-    texts.extend(text_splitter.split_documents(result))
-chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=False)
-print(chain.run(texts))
+print(generate_summary(youtube_url_list))
