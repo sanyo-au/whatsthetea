cd0cf81584a398e0a139444adc1c76e1848f31d8
Sai Shruthi Balaji <shruthibalaji2307@gmail.com>
2024-04-20 14:13:29 -0700

    Added image generation

--- whatsthetea.py
+++ whatsthetea.py
@@ -6,10 +6,26 @@ import os
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
+
+import io
+import cv2
+
 # SETUP
 # Set env variable using export OPENAI_API_KEY=<your_key>
 api_key = os.environ['OPENAI_API_KEY']
-llm = OpenAI(temperature=0, openai_api_key=api_key)
+
+# Set up the models
+text_llm = OpenAI(name="gpt-3.5-turbo", temperature=0, openai_api_key=api_key)
+text_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a newsletter reporter. Everything you say will sound like it's meant for a newsletter. Only use the information from the video. Start by summarizing the video."),
+    ("user", "{input}")
+])
+
+def generate_image(summary):
+    image_url = DallEAPIWrapper().run(summary)
+    return image_url
 
 def generate_summary(youtube_url_list):
     texts = []
@@ -21,8 +37,13 @@ def generate_summary(youtube_url_list):
         result = loader.load()
 
         texts.extend(text_splitter.split_documents(result))
-    chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=False)
+    chain = load_summarize_chain(text_llm, chain_type="map_reduce", verbose=False)
     return chain.run(texts)
 
-youtube_url_list = ["https://www.youtube.com/watch?v=AXq0QHUwmh8", "https://www.youtube.com/watch?v=EwHrjZxAT7g"]
-print(generate_summary(youtube_url_list))
+youtube_url_list = ["https://www.youtube.com/watch?v=ytdIjfGuHZQ"]
+summary = generate_summary(youtube_url_list)
+print("Text summary:")
+print(summary)
+image_url = generate_image(summary)
+print("Image URL:")
+print(image_url)
