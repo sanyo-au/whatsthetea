44ddf98228931e3e6dcb3ff116a68a51fd4d8ffc
sanyo-au <sanyogeeta@autonomic.com>
2024-04-20 14:08:20 -0700

    Add email capability

---
+++ sendemail.py
@@ -0,0 +1,24 @@
+import base64
+from email.mime.text import MIMEText
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from requests import HTTPError
+
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.send"
+]
+flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+creds = flow.run_local_server(port=0)
+
+service = build('gmail', 'v1', credentials=creds)
+message = MIMEText('This is the body of the email')
+message['to'] = 'sanyogeetalawande@gmail.com'
+message['subject'] = 'Email Subject'
+create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+try:
+    message = (service.users().messages().send(userId="me", body=create_message).execute())
+    print(F'sent message to {message} Message Id: {message["id"]}')
+except HTTPError as error:
+    print(F'An error occurred: {error}')
+    message = None
